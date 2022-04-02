04c4ee805ffdad02e55250ef33ab92b1335a7e7e
Nico Van den Hooff <nicovandenhooff@gmail.com>
2022-04-02 12:54:13 -0700

    update api'

--- api/api.py
+++ api/api.py
@@ -1,12 +1,16 @@
 import time
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
+CORS(app)
 
 @app.route('/api/time')
+@cross_origin()
 def get_current_time():
     return {'time': time.time()}
 
 @app.route('/')
+@cross_origin()
 def index():
     return app.send_static_file('index.html')
